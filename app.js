6278d7c5996b987e268577bbb004189a4efba4cf
Chris Gross <cjg28@papayas.(none)>
2012-04-11 16:48:04 -0400

    Simple proof of concept. Pulls all text columns from db

---
+++ app.js
@@ -0,0 +1,24 @@
+var _mysql = require('mysql');
+
+var MYSQL_USER = 'root';
+var MYSQL_PASS = 'password';
+var DATABASE = 'todo';
+var TABLE = 'gadgets';
+
+var mysql = _mysql.createClient({
+    user: MYSQL_USER,
+    password: MYSQL_PASS,
+});
+
+mysql.query('use ' + DATABASE);
+
+mysql.query('select * from helloWorld',
+function(err, result, fields) {
+    if (err) throw err;
+    else {
+        for (var i in result) {
+            var item = result[i];
+            console.log(item.text);
+        }
+    }
+});
