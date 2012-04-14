e966cf82bda5d629b9f2cc5210b7566c73bdc343
Jeff <chien-hung.chen@case.edu>
2012-04-14 01:16:35 -0400

    testing node

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 //Test.js is the main test file that does basic tests of the functionality of sanitizer.js for development sanity
 
-var sanitizer = require('sanitizer');
+//var sanitizer = require('sanitizer');
 
 console.log("test");
