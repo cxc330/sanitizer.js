ad051e2c1627eb6c68d0ebb260b44ef7e989a3cc
Jeff <chien-hung.chen@case.edu>
2012-04-14 01:36:01 -0400

    testing require in node

--- sanitizer.js
+++ sanitizer.js
@@ -1,6 +1,7 @@
 //Created By: Chien-Hung Chen, Christopher Gross, Davis Wilkinson, Alex Sevrain
 //The code is provided AS-IS and is free to distribute, edit, and reuse with mentions of the original developers
 
-exports.test = function(stringIn){
-	return (stringIn);
+exports.stripHTML = function(stringIn){
+	var matches = stringIn.match(new RegExp(/<[^>]*>/));
+	return ("NOT DONE");
 };
