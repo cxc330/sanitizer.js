da8c2b703170d81ee44d4f8a839837276eba15f8
Jeff <chien-hung.chen@case.edu>
2012-04-14 01:22:36 -0400

    testing require in node

--- sanitizer.js
+++ sanitizer.js
@@ -1,8 +1,9 @@
 //Created By: Chien-Hung Chen, Christopher Gross, Davis Wilkinson, Alex Sevrain
 //The code is provided AS-IS and is free to distribute, edit, and reuse with mentions of the original developers
 
+var count = 0;
 
 
 exports.test = function(){
-	return "testing inside sanitizer.js";
+	return (count++);
 }
