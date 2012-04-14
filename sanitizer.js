5aaeb61a0363174b0cd3db56b02ccf4c40e95245
Jeff <chien-hung.chen@case.edu>
2012-04-14 01:08:45 -0400

    commiting adding package.json and test.js

--- sanitizer.js
+++ sanitizer.js
@@ -1,1 +1,4 @@
-alert("test");
+//Created By: Chien-Hung Chen, Christopher Gross, Davis Wilkinson, Alex Sevrain
+//The code is provided AS-IS and is free to distribute, edit, and reuse with mentions of the original developers
+
+
