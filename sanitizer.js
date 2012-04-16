49b34740b19b79c8ec234215f1cd5e86cacd858b
Jeff <chien-hung.chen@case.edu>
2012-04-16 01:24:44 -0400

    added sample email function

--- sanitizer.js
+++ sanitizer.js
@@ -4,3 +4,7 @@
 exports.stripHTML = function(stringIn){
 	return stringIn.replace(/<(?:.|\n)*?>/gm, '');
 };
+
+exports.verifyEmail = function(stringIn){
+	return (stringIn.search(/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/g) == 0);
+};
--- test.js
+++ test.js
@@ -3,7 +3,27 @@
 var sanitizer = require('./sanitizer.js');
 
 
-//Variables for testing
+/*Variables for testing
+------------------------------------------------------------*/
+
+//emails
+var goodEmails = [];
+goodEmails.push("niceandsimple@example.com");
+goodEmails.push("simplewith+symbol@example.com");
+goodEmails.push("less.common@example.com");
+goodEmails.push( "a.little.more.unusual@dept.example.com");
+goodEmails.push("'@[10.10.10.10]");
+goodEmails.push("user@[IPv6:2001:db8:1ff::a0b:dbd0]");
+goodEmails.push( "\"much.more\ unusual\"@example.com");
+goodEmails.push( "\"very.unusual.@.unusual.com\"@example.com");
+goodEmails.push( "\"very.(),:;<>[]\\\".VERY.\\\"very@\\\ \\\"very\\\".unusual\\\"@strange.example.com");
+goodEmails.push( "0@a");
+goodEmails.push( "!#$%&'*+-/=?^_`{}|~@example.org");
+goodEmails.push( "\"()<>[]:;@,\\\\\"!#$%&'*+-/=?^_`{}|\ \ \ \ \ ~\ \ \ \ \ \ \ ?\ \ \ \ \ \ \ \ \ \ \ \ ^_`{}|~.a\"@example.org");
+goodEmails.push( "\"\"@example.org");
+goodEmails.push( "postbox@com");
+
+//HTML
 var plainHTML = "<body>"+
 	"<div class=\"wrapper\">"+
 		"<div class=\"header\">"+
@@ -17,27 +37,45 @@ var plainHTML = "<body>"+
 			"<li><a href=\"/education/\"><span class=\"menuLong\">Education</span><span class=\"menuShort\">Edu</span></a></li>"+
 		"</ul>"+
 	"</div>"+
-"</div>"+
-"<div class=\"line\"></div>		<div class=\"main\">"+
-    		"<h1 class=\"grayCopperplate\">About Me</h1>"+
-			"<div class=\"contents\">"+
-        		"<p>Hi, I'm Chien-Hung (Jeff) Chen, and I am a Computer Science student currently attending Case Western Reserve University. I will be receiving a Bachelors in Computer Science in May 2012 and a Masters in Computer Science in May 2013.</p>"+
-                "<p>I have had an array of experiences in web development using Javascript, HTML5 and CSS3, C#.NET, VB.NET, PHP, Ruby On Rails. I have also had experience developing applications in C#, Java, and C. Lastly, I have also worked with a variety of databases including MySQL, PostgreSQL, SQL Server, and Oracle Database.</p>"+
-                "<p>In the past year, I have recently started mobile development using Titanium SDK and Corona SDK and has deployed applications to iOS and Android devices.</p>"+
-                "<p>Currently, I am splitting my time pursuing my BS/MS degrees, working at Formfire as a web developer, developing research projects at the Center of Clinical Investigation at Case Western Reserve University, and doing side projects as well (see my github and portfolio).</p>"+
-    		"</div>"+
-			"<div class=\"lightline\"></div>"+
-    		"<div class=\"footer\">"+
-	"<a href=\"/resume/\" class=\"top\">Resume</a>"+
-	"<a href=\"/contact/\" class=\"top\">Contact</a>"+
-"</div>		</div>"+
-	"</div>"+
-"</body>";
+"</div>";
+
+var scriptsInHTML = "<head>"+
+        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"+
+        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />"+
+        "<link rel=\"icon\" href=\"/assets/images/favicon.ico\">"+
+        "<script language=\"javascript\" type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js\"></script>"+
+        "<!--[if lt IE 9]>"+
+	        "<script src=\"http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js\"></script>"+
+        "<![endif]-->"+
+        "<script type=\"text/javascript\">"+
+          "var _gaq = _gaq || [];"+
+          
+          "_gaq.push(['_setAccount', 'UA-30879788-1']);"+
+          "_gaq.push(['_trackPageview']);"+
+          
+          "(function() {"+
+            "var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;"+
+            "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';"+
+            "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);"+
+          "})();"+
+        "</script>"+
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/css/style.css\" />	<title>Home - Chien-Hung Chen</title>"+
+"</head>";
+
+/*Calling sanitizer.js to test
+------------------------------------------------------------*/
+
+//Testing Email
+console.log("\n----------Testing Email----------\n");
+for(int i = 0; i < goodEmails.length; i++)
+{
+	if(sanitizer.verifyEmail(goodEmails[i]))
+		console.log("Good: " + goodEmails[i]);
+	else
+		console.log("Bad: " + goodEmails[i]);
+}
 
 //Testing HTML
-console.log("----------Testing HTML----------");
+console.log("\n----------Testing HTML----------\n");
 console.log(sanitizer.stripHTML(plainHTML));
-console.log(sanitizer.stripHTML("IS"));
-console.log(sanitizer.stripHTML("A"));
-console.log(sanitizer.stripHTML("BAD"));
-console.log(sanitizer.stripHTML("STRING"));
+console.log(sanitizer.stripHTML(scriptsInHTML));
