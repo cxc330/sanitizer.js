564aa030fb9139b4887ec76445d7b9c0e50d1b14
Chris Gross <cjg28@papayas.(none)>
2012-04-17 16:21:34 -0400

    Flushed everything out and you can now insert. Needs to have sanitized input and need to check out scope in order to cache previous request.

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var express = require('express');
 app = express.createServer();
 var mysql;
 var mysqlConfig = require('./mysqlConfig');
+var queryData;
+var currentDate = "CURDATE()";
+var notComplete = 0;
+var $ = require('jquery');
 
 app.configure(function(){
-	app.use(express.static(__dirname + '/static'));
+	app.use(express.static(__dirname + '/static')); //static file serving
+	app.use(express.bodyParser()); //post data parsing
 });
 
 app.set('views', __dirname + '/views');
@@ -32,7 +37,7 @@ mySqlStart();
 
 //Default route
 app.get('/todo', function(req, res){
-	var response = {query:[]};
+	queryData = {query:[]};
 	mysql.query('select * from ' + TABLE,
 	function(err, result, fields) {
 		if (err) throw err;
@@ -40,7 +45,7 @@ app.get('/todo', function(req, res){
 			for (var i in result) {
 				var item = result[i];
 				console.log('Query result ' + i + ': ' + item.task);
-				response.query.push({					
+				queryData.query.push({					
 						'task'			: item.task,
 						'creationDate'	: item.creationDate, 
 						'dueDate'		: item.dueDate,
@@ -49,55 +54,89 @@ app.get('/todo', function(req, res){
 			}
 		}
 		console.log('Query complete');
-		res.render('todo', {results: response });
+		res.render('todo', {results: queryData });
 	});
 });
 
 app.post('/todoRequest', function(req, res){
- console.log(req.body);
- res.send(req.body);	
+ processData(req.body);
+ res.redirect('todo');	//redirect back to home page
 });
 
 //404 ROUTE
 app.get('*', function(req, res){
   res.render('404', 404);
 });
-/*
-app.get('/todo', function(req, res, next){
-	console.log("something");
-	fs.createWriteStream("todo/index.html", {
-    		flags: "a",
-    		encoding: "encoding",
-    		mode: 0666
-	}).write("appended text\n");
-	console.log('todo request');
-	
-});
-/*
-app.post('/todoRequest', function(req, res){
-	var response = {query:[{}]};
-	
-  	mysql.query('select * from ' + TABLE,
-	function(err, result, fields) {
-		if (err) throw err;
-		else {
-			for (var i in result) {
-				var item = result[i];
-				console.log('Query result ' + i + ': ' + item.task);
-				response.query.push({
-						/*creationDate	: item.creationDate, 
-						dueDate			: item.dueDate,
-						task			: item.task
-				});
-				console.log(response.query[1].task);
-			}
-		}
-		console.log('Query complete');
-	});
-	
-	console.log(response.query);
-	res.send(response);
-});*/
 
+function processData(data){
+	console.log(data);
+	
+	var newDue = $.trim(data.newDue);
+	var newTask = $.trim(data.newTask);
+	
+	if (newTask != '')
+	{
+		insertData(currentDate, "'" + newDue + "'", "'" + newTask+ "'", notComplete);
+	}
+	
+	var x = 0;
+	for (var attribute in data) //check each data attribute
+	{
+		console.log(attribute + ": " + data[attribute]);
+		var propOne = data[attribute];
+		var propTwo = queryData;
+		if (propOne == propTwo)
+			console.log("No change");
+		else
+			console.log("change between " + propOne + " and " + propTwo);
+		x++;
+	}
+}
+
+function insertData (createDate, dueDate, task, complete){
+	
+	var query = "INSERT INTO " + TABLE + " (creationDate, dueDate, task, complete)";
+	query += " VALUES (" + createDate + ", " + dueDate + ", " + task + ", " + complete +")";
+	
+	mysql.query(query,
+		function(err, result, fields) {
+			if (err) 
+				throw err;
+    		else 
+				console.log('Inserted into DB using the following statement:\n\t "' + query + '"');
+		});
+}
+
+function updateData(createDate, dueDate, task, complete, set)
+{
+	var query = "UPDATE " + TABLE + " SET " + set + " WHERE task = '" + task + "'";
+	query += "AND creationDate = '" + createDate + "'";
+	query += "AND dueDate = '" + dueDate + "'";
+	query += "AND complete = '" + complete +"'";
+	
+	mysql.query(query,
+		function(err, result, fields) {
+			if (err) 
+				throw err;
+    		else 
+				console.log('Updated DB using the following statement:\n\t "' + query + '"');
+		});
+}
+	
+function deleteData (createDate, dueDate, task, complete)
+{
+	var query = "DELETE FROM " + TABLE + " WHERE task = '" + task + "'";
+	query += "AND creationDate = '" + createDate + "'";
+	query += "AND dueDate = '" + dueDate + "'";
+	query += "AND complete = '" + complete +"'";
+	
+	mysql.query(query,
+		function(err, result, fields) {
+			if (err) 
+				throw err;
+    		else 
+				console.log('Deleted from DB using the following statement:\n\t "' + query + '"');
+		});
+}
 
 app.listen(3000);
